db4014f33bbaafc1c82092ddd33e40aa29bdaf22
georgia544 <diana.shakh98@gmail.com>
2023-11-27 16:34:28 +0300

    done

--- app/src/main/java/com/hfad/tasks/TasksFragment.kt
+++ app/src/main/java/com/hfad/tasks/TasksFragment.kt
@@ -26,6 +26,7 @@ class TasksFragment:Fragment() {
         val viewModel = ViewModelProvider(
             this,viewModelFactory).get(TasksViewModel::class.java)
         binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
 
         return view
     }
